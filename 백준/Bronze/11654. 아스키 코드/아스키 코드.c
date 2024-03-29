2472d6f67e113dc38607f736cd84040a12edf23b
Kchanghyun <154062748+Kchanghyun@users.noreply.github.com>
2024-03-30 01:43:21 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+	char asc;
+	scanf("%c", &asc);
+	printf("%d\n", asc);
+	return 0;
+}
