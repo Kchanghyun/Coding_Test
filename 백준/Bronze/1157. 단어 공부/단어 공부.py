312f00b4ed91de007b54bcd0a0ea098e2bf9cfa8
Kchanghyun <154062748+Kchanghyun@users.noreply.github.com>
2024-04-06 04:54:37 +0900

    [Bronze I] Title: 단어 공부, Time: 76 ms, Memory: 33076 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,18 +1,11 @@
-word = input()
-word = word.lower()
-a = [0] * 26
-for i in range(len(word)):
-    a[ord(word[i]) - 97] += 1
-# print(a.index(max(a)))
-# max(a)는 가장 큰 값의 첫 번째 인덱스의 값을 반환한다.
-duplication = 0
-for i in range(0, len(a)):
-    if i == a.index(max(a)):
-        continue
-    if a[i] == max(a):
-        duplication = -1
+word = input().lower()
+word_list = list(set(word))
+cnt = []
+for i in word_list:
+    count = word.count(i)
+    cnt.append(count)
 
-if duplication == -1:
+if cnt.count(max(cnt)) >= 2:
     print("?")
 else:
-    print(chr(a.index(max(a)) + 65))
+    print(word_list[(cnt.index(max(cnt)))].upper())
