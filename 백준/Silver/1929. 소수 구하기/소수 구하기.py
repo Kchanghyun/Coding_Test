309f83b7a97e81ecc935b47cbe9e67d6c072b018
Kchanghyun <154062748+Kchanghyun@users.noreply.github.com>
2024-05-19 22:05:32 +0900

    [Silver III] Title: 소수 구하기, Time: 5304 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,13 +1,10 @@
-import math
 m, n = map(int, input().split())
 
 for i in range(m, n+1):
     if i == 1:
         continue
-    error = 0
-    for j in range(2, int(math.sqrt(i)) + 1):
+    for j in range(2, int(i**0.5) + 1):
         if i % j == 0:
-            error = 1
             break
-    if error == 0:
+    else: # 위의 for문이 break 없이 끝났다면 (즉, i가 소수라면)
         print(i)
