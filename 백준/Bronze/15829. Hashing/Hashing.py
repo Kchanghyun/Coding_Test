4ad99afef45c499b49692af48ea3e4d257b3013a
김창현 <154062748+Kchanghyun@users.noreply.github.com>
2024-07-16 15:45:13 +0900

    [Bronze II] Title: Hashing, Time: 44 ms, Memory: 31120 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,12 @@
+import sys
+
+# a ~ z = 1 ~ 26
+n = int(sys.stdin.readline())
+string = sys.stdin.readline()
+
+ret = 0
+for i in range(n): # 0 ~ n-1
+    alphabet = ord(string[i]) - 96
+    ret += (alphabet * 31**i)
+
+print(ret)
