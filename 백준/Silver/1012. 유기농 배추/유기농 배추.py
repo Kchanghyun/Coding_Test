f141701b8013d1d7c399e04e6ea2159a7c88fc9e
김창현 <154062748+Kchanghyun@users.noreply.github.com>
2024-08-09 22:08:22 +0900

    [Silver II] Title: 유기농 배추, Time: 72 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,40 +1,41 @@
-T = int(input()) #테스트케이스의 개수
+import sys
+from collections import deque
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
 
-def BFS(x,y):           
-    queue = [(x,y)]
-    matrix[x][y] = 0 # 방문처리
+def bfs(x, y):
+    queue = deque([(x, y)])
+    farm[x][y] = 0  # 방문처리
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
 
     while queue:
-        x,y = queue.pop(0)
+        cx, cy = queue.popleft()
 
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+            nx = cx + dx[i]
+            ny = cy + dy[i]
 
-            if nx < 0 or nx >= M or ny < 0 or ny >= N:
+            if nx < 0 or nx >= m or ny < 0 or ny >= n:
                 continue
+            if farm[nx][ny] == 1:
+                farm[nx][ny] = 0
+                queue.append((nx, ny))
 
-            if matrix[nx][ny] == 1 :
-                queue.append((nx,ny))
-                matrix[nx][ny] = 0
 
-# 행렬만들기
-for i in range(T):
-    M, N, K = map(int,input().split())
-    matrix = [[0]*(N) for _ in range(M)]
-    cnt = 0
-
-    for j in range(K):
-        x,y = map(int, input().split())
-        matrix[x][y] = 1
-
-    for a in range(M):
-        for b in range(N):
-            if matrix[a][b] == 1:
-                BFS(a,b)
-                cnt += 1
-
-    print(cnt)
+t = int(sys.stdin.readline())
+for i in range(t):
+    n, m, k = map(int, sys.stdin.readline().split())
+    # points = []
+    farm = [[0] * n for _ in range(m)]  # arr = [[0] * cols for _ in range(rows)] row = 행, col = 열
+    # n개의 m짜리 리스트를 생성하는 코드라고 생각.
+    snake = 0
+    for j in range(k):
+        x, y = map(int, sys.stdin.readline().split())
+        # points.append((b, a))
+        farm[y][x] = 1
+    for a in range(m):
+        for b in range(n):
+            if farm[a][b] == 1:
+                bfs(a, b)
+                snake += 1
+    print(snake)
