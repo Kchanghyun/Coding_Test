ebfece1d4fd51b845c7d7a83803a8d74db7db11b
김창현 <154062748+Kchanghyun@users.noreply.github.com>
2024-08-11 03:22:11 +0900

    [Silver III] Title: 2×n 타일링, Time: 36 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,7 +1,9 @@
-n = int(input())
-arr = [0, 1, 2]
+import sys
 
-for i in range(3, n + 1):
-    arr.append(arr[i - 1] + arr[i - 2])
+n = int(sys.stdin.readline())
+dp = [0, 1, 2]
 
-print(arr[n] % 10007)
+for i in range(3, n+1):
+    dp.append(dp[i-1] + dp[i-2])
+
+print(dp[n] % 10007)
