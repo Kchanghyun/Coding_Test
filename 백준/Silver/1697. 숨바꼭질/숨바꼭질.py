138611742bb1d54158d575aa63a02740bcf095ec
김창현 <154062748+Kchanghyun@users.noreply.github.com>
2024-08-20 02:28:45 +0900

    [Silver I] Title: 숨바꼭질, Time: 80 ms, Memory: 35296 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,24 +1,24 @@
+import sys
 from collections import deque
 
-n, k = map(int, input().split())
+N, K = map(int, sys.stdin.readline().split())
 
+# visited = {} 
+# set이나 dictionary를 사용하면 메모리 사용량이 많다. 메모리 양이 적어야 할 때는 list를 쓰기
 
-def solution(n, k):
-    if n >= k:
-        return n - k
-
+def bfs(n):
     q = deque()
-    q.append([0, n])
-    visited = [False] * (k * 2 + 1)
+    q.append(n)
+    MAX = 10 ** 5
+    visited = [0] * (MAX+1)
+
     while q:
-        cost, pos = q.popleft()
-        if pos == k:
-            return cost
-
-        for nxt in (pos - 1, pos + 1, pos * 2):
-            if 0 <= nxt <= k + 1 and not visited[nxt]:
-                visited[nxt] = True
-                q.append((cost + 1, nxt))
-
-
-print(solution(n, k))
+        cur = q.popleft()
+        if cur == K:
+            return visited[K]
+        
+        for i in (cur-1, cur+1, cur*2):
+            if 0 <= i <= MAX and not visited[i]:
+                visited[i] = visited[cur] + 1
+                q.append(i)
+print(bfs(N))
