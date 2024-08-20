6877a78620e518ca7b68ad0314c52f6ef4becb72
김창현 <154062748+Kchanghyun@users.noreply.github.com>
2024-08-20 19:35:23 +0900

    [Silver I] Title: 숨바꼭질, Time: 72 ms, Memory: 34632 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -3,22 +3,31 @@ from collections import deque
 
 N, K = map(int, sys.stdin.readline().split())
 
-# visited = {} 
-# set이나 dictionary를 사용하면 메모리 사용량이 많다. 메모리 양이 적어야 할 때는 list를 쓰기
+# 수빈 위치 : N
+# 동생 위치 : K
 
-def bfs(n):
-    q = deque()
-    q.append(n)
+# 1. 걸을 때 : x+1 or x-1
+# 2. 순간이동 : 2x
+
+# 동생 찾을 수 있는 가장 빠른 시간
+
+def bfs(n, k):
+    if n >= k:
+        return n-k
+    
+    queue = deque()
+    queue.append((n))
     MAX = 10 ** 5
-    visited = [0] * (MAX+1)
+    visited = [0] * (MAX + 1)
 
-    while q:
-        cur = q.popleft()
-        if cur == K:
-            return visited[K]
+    while queue:
+        cur = queue.popleft()
+        if cur == k:
+            return visited[k]
         
         for i in (cur-1, cur+1, cur*2):
             if 0 <= i <= MAX and not visited[i]:
                 visited[i] = visited[cur] + 1
-                q.append(i)
-print(bfs(N))
+                queue.append(i)
+    
+print(bfs(N, K))
