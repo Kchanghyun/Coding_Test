f7099d3c699b34436b4614b48eecf989bd358b8b
김창현 <154062748+Kchanghyun@users.noreply.github.com>
2024-09-14 15:25:18 +0900

    [Gold V] Title: 토마토, Time: 1672 ms, Memory: 57552 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,19 +1,19 @@
 import sys
 from collections import deque
 
-m, n, h = map(int, sys.stdin.readline().split())
+m, n, h = map(int,sys.stdin.readline().split())
 
 matrix = [[list(map(int, sys.stdin.readline().split())) for _ in range(n)] for _ in range(h)]
-visited = [[[False]*m for _ in range(n)] for _ in range(h)]
-
-dx = [1, -1, 0, 0, 0, 0]
-dy = [0, 0, 1, -1, 0, 0]
-dz = [0, 0, 0, 0, 1, -1]
+visited = [[[False] * m for _ in range(n)] for _ in range(h)]
 
 queue = deque()
+dx = [1,-1,0,0,0,0]
+dy = [0,0,1,-1,0,0]
+dz = [0,0,0,0,1,-1]
+
 
 def bfs():
-    while(queue):
+    while queue:
         x, y, z = queue.popleft()
         for i in range(6):
             nx = x + dx[i]
@@ -25,21 +25,23 @@ def bfs():
                 matrix[nx][ny][nz] = matrix[x][y][z] + 1
                 queue.append((nx, ny, nz))
 
-answer = 0
 for i in range(h):
     for j in range(n):
         for k in range(m):
             if matrix[i][j][k] == 1 and not visited[i][j][k]:
                 queue.append((i, j, k))
                 visited[i][j][k] = True
+
 bfs()
 
+
+ret = 0
 for a in matrix:
     for b in a:
         for c in b:
             if c == 0:
                 print(-1)
                 exit(0)
-        answer = max(answer, max(b))
+        ret = max(ret, max(b))
 
-print(answer-1)
+print(ret-1)
