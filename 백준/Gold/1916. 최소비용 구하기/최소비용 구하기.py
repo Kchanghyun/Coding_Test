a61f302ba2da5673cf60221a29bf77521e8465b2
김창현 <154062748+Kchanghyun@users.noreply.github.com>
2025-12-18 21:59:04 +0900

    [Gold V] Title: 최소비용 구하기, Time: 264 ms, Memory: 58232 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -1,32 +1,51 @@
 import sys
-import heapq 
+import heapq
 
 input = sys.stdin.readline
 
-n = int(input())
-m = int(input())
-graph = [[] for _ in range(n+1)]
+N = int(input())
+M = int(input())
 
-for _ in range(m):
-    a, b, cost = map(int, input().split())
-    graph[a].append([b, cost])
-        
-start, end = map(int, input().split())
-costs = [1e9 for _ in range(n+1)]
-heap = []
-costs[start] = 0
-heapq.heappush(heap, [0, start])
-    
-while heap:
-    cur_cost, cur_v = heapq.heappop(heap)
-    if costs[cur_v] < cur_cost:
-        continue
-    for next_v, next_cost in graph[cur_v]:
-        sum_cost = cur_cost + next_cost
-        if sum_cost >= costs[next_v]:
+graph = [[] * (N+1) for _ in range(N+1)]
+visited = [False] * (N+1)
+INF = 1e8
+distances = [INF] * (N+1)
+
+for i in range(M):
+    s, e, w = map(int, input().split())
+    graph[s].append((e, w))
+
+s, e = map(int, input().split())
+
+"""
+시간제한 0.5초
+대충 1억번 연산인가? 파이썬은 그보다 좀 느리니까 5천번??
+
+N = 1000
+M = 100000
+
+A번째 도시에서 B번째 도시까지 가는데 드는 최소 비용
+s에서 e까지 가는데 드는 최소 비용
+"""
+
+def dijkstra(graph, start):
+    distances[start] = 0
+    pq = [(0, start)] # 시작점 설정
+
+    while pq:
+        current_distance, current_node = heapq.heappop(pq)
+
+        if current_distance > distances[current_node]:
             continue
-        
-        costs[next_v] = sum_cost
-        heapq.heappush(heap, [sum_cost, next_v])
-        
-print(costs[end])
+
+        for neighbor, weight in graph[current_node]:
+            distance = current_distance + weight
+
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(pq, (distance, neighbor))
+    return distances
+
+result = dijkstra(graph, s)
+
+print(result[e])
